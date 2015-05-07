50ce24ac8f1a111070f43c4751fdbac036ae93d2
DwiteeKP <dwiteekp@KAI-MAC59.local>
2015-05-07 15:59:22 +0530

    Initial Commit

---
+++ MineSweeper/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  MineSweeper
+//
+//  Created by DwiteeKP on 07/05/15.
+//  Copyright (c) 2015 DwiteeKP. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
